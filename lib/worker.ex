bea2fa89dc9e685c6b09a9ce335ec2c559dfc0e4
Claudio Yuri <claudio.yuri@gmail.com>
2017-05-22 22:54:31 -0300

    GenServer Added
    * Client-server operations now handled via GenServer
    * Added the stats request

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,25 +1,52 @@
 defmodule Metex.Worker do
-    def loop do
-        receive do
-            {sender_pid, location} ->
-                send(sender_pid, {:ok, temperature_of(location)})
-            _ ->
-                IO.puts "don´t know how to process this message"
-        end
-        loop()
+    use GenServer
+    ## Client API
+    def start_link(opts \\ []) do
+        # staritng a link makes the spawned process to inform the spawner in case of failure
+        GenServer.start_link(__MODULE__, :ok, opts)
     end
 
-    def temperature_of(location) do
-        result = url_for(location) |> HTTPoison.get |> parse_response
-        case result do
+    def get_temperature(pid, location) do
+        # makes a synchronous call
+        GenServer.call(pid, {:location, location})
+    end
+    
+    def get_stats(pid) do
+        GenServer.call(pid, :get_stats)
+    end
+    def reset_stats(pid) do
+        GenServer.cast(pid, :reset_stats)
+    end
+
+    ## Server Callbacks
+    def init(:ok) do
+        {:ok, %{}}
+    end
+
+    # handles a request tagged with the :location atom
+    # _from has a tuple with the client's pid and a unique message tag 
+    def handle_call({:location, location}, _from, stats) do
+        case temperature_of(location) do
             {:ok, temp} ->
-                "#{location}: #{temp}°C"
-            :error ->
-                "#{location} not found"
+                new_stats = update_stats(stats, location)
+                {:reply, "#{temp}°C", new_stats}
+            _ ->
+                {:reply, :error, stats}
         end
     end
+
+    def handle_call(:get_stats, _from, stats) do
+        {:reply, stats, stats}
+    end
+    def handle_cast(:reset_stats, _stats) do
+        {:noreply, %{}}
+    end
+
+    ## Helper Functions
+    defp temperature_of(location) do
+        url_for(location) |> HTTPoison.get |> parse_response
+    end
     defp url_for(location) do
-        location = URI.encode(location)
         "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
     end
     defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
@@ -36,17 +63,16 @@ defmodule Metex.Worker do
             _ -> :error
         end
     end
-    defp apikey do
+    def apikey do
         "76c0b2c75a11958bc1a03989ddeeba48"
     end
-end
-
-defmodule Metex do
-    def temperatures_of(cities) do
-        coordinator_pid = spawn(Metex.Coordinator, :loop, [[], Enum.count(cities)])
-        cities |> Enum.each(fn city ->
-            worker_pid = spawn(Metex.Worker, :loop, [])
-            send worker_pid, {coordinator_pid, city}
-        end)
+    defp update_stats(old_stats, location) do
+        case Map.has_key?(old_stats, location) do
+            true ->
+                # same as Map.update!(old_stats, location, fn(val) -> val + 1 end)
+                Map.update!(old_stats, location, &(&1 + 1))
+            false ->
+                Map.put_new(old_stats, location, 1)
+        end
     end
 end
